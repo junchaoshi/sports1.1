1d05697cbdda24a11050aab8e8b80bd1900246fc
junchaoshi <30919573+junchaoshi@users.noreply.github.com>
2019-08-22 17:07:53 -0700

    Add files via upload

--- source/tRNA_mapping.pl
+++ source/tRNA_mapping.pl
@@ -15,10 +15,6 @@ my $in_file = shift or die $usage;
 
 my $summary = shift or die $usage;
 
-
-open INPUT1, $in_file
-	or die "Can't open '$in_file': $!";
-
 my $total_reads;
 {
 	open INPUT2, $summary
@@ -29,22 +25,33 @@ my $total_reads;
 	close INPUT2;
 }
 
-my ($read, $annotation, $start_site, $seq, $repeat_site, @annos, $temp_anno);
-my (%ref_lens, %reads);
+my ($id, $read, $annotation, $start_site, $seq, @annos, $temp_anno);
+my (%ref_lens, %reads, %repeat_sites);
 
+open INPUT1, $in_file
+	or die "Can't open '$in_file': $!";
 while (<INPUT1>){
-	($read, $annotation, $start_site, $seq, $repeat_site) = (split /\t/, $_)[1, 3, 4, 5, 7];
+	$id = (split /\t/, $_)[0];
+	$repeat_sites{$id}++;
+}
+close INPUT1;
+
+open INPUT1, $in_file
+	or die "Can't open '$in_file': $!";
+while (<INPUT1>){
+	($id, $read, $annotation, $start_site, $seq) = (split /\t/, $_)[0, 1, 3, 4, 5];
 	@annos = split(/\s+/, $annotation);
 	$annos[0] =~ /(.+)-.+$/;
 	$temp_anno = $1;
-	$ref_lens{$temp_anno} = $annos[-4];
+	$annotation =~ / ([0-9]+) bp/;
+	$ref_lens{$temp_anno} = $1;
 	for (my $i = $start_site; $i < $start_site + (length $seq); $i++ ){
-		$reads{$temp_anno}[$i] += $read / ($repeat_site + 1) *1000000 / $total_reads;
+		$reads{$temp_anno}[$i] += $read / $repeat_sites{$id} *1000000 / $total_reads;
 	}
 }
 
 foreach my $uniq_id (sort keys %ref_lens){
-	for (my $i = 0; $i < ($ref_lens{$uniq_id} +3); $i++){
+	for (my $i = 0; $i < ($ref_lens{$uniq_id}); $i++){
 		unless ($reads{$uniq_id}[$i]){
 			$reads{$uniq_id}[$i] = 0;
 		}
